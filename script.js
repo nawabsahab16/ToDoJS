e947bc720508961c33a599294377ba6ce1dfadf9
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-04-11 11:58:38 +0530

    added addTask

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputBox = document.getElementById("input-box");
+
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    
+    if(inputBox.value === '' ){
+        alert("You have to write something! ");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+
+}
